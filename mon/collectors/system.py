ccb54d8bca11494dd8c19be1e331983fab20c374
Andreas Schroeder <andreas@a-netz.de>
2018-10-06 08:45:11 +0800

    Added memory info

--- mon/collectors/system.py
+++ mon/collectors/system.py
@@ -1,3 +1,4 @@
+import re
 import socket
 
 from mon.classreg import register_collector_class
@@ -63,3 +64,54 @@ class HostInfo(CollectorBase):
         }
 
 register_collector_class(HostInfo)
+
+
+class MemoryInfo(CollectorBase):
+    def __init__(self, cfg):
+        super().__init__(
+            cfg=cfg,
+            interval=20,
+            namespace='system')
+
+        
+    def _get_meminfo(self):
+        lines = self._get_file_data(
+            filename='/proc/meminfo').split('\n')
+
+        meminfo = {}
+        
+        for line in lines:
+            m = re.match('^([^:]+):\s*(\d+)( kB)?$', line)
+            if m:
+                meminfo[m.group(1)] = int(m.group(2))
+
+        return meminfo
+
+    
+    def _get_values(self):
+        meminfo = self._get_meminfo()
+        
+        # Calculation according to htop author:
+        # http://stackoverflow.com/questions/41224738/how-to-calculate-
+        #  memory-usage-from-proc-meminfo-like-htop
+
+        data = {}
+        
+        # All the memory used by the system.
+        data['totalUsed'] = (meminfo['MemTotal'] - meminfo['MemFree']);
+
+        # The memory the system can release whenever necessary and uses for
+        # caching.
+        data['cached'] = (
+            meminfo['Cached'] + meminfo['SReclaimable'] - meminfo ['Shmem'])
+
+        # Memory actually in use by processes. 
+        data['inUse'] = (
+            data['totalUsed'] - meminfo['Buffers'] - data['cached'])
+
+        # Percent of memory used.
+        data['pctInUse'] = float(data['inUse']) / meminfo['MemTotal']
+        
+        return data;
+
+register_collector_class(MemoryInfo)
